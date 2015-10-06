5a9ef6a0dd40121d7d4204db9260dfaa87b2f371
Nedelin Yordanov <nedeliny@gmail.com>
2015-10-07 01:59:48 +0300

    Add melee attack swing prototype(WIP)

--- server/systems/entities/abilities/meleeattack.js
+++ server/systems/entities/abilities/meleeattack.js
@@ -1,20 +1,50 @@
 var Event = require('../../../lib/event.js');
 var consts = require('../../../lib/const.js');
+var Vec2d = require('../../../lib/vectormath.js');
 
 function MeleeAttack (entity) {
   this.entity = entity;
   this.name = "basic-melee-attack";
   this.unitTarget = -1;
 
+  this.attackRange = 50;
+  this.attackTime = 300; //ms
+  this.attackSwingId = -1;
+
   this.subscribedEvents = {};
 
   this.subscribedEvents[this.entity.eventDispatcher.id] = {};
   this.subscribedEvents[this.entity.eventDispatcher.id][consts.EVENT_ENTITY_STATE_CHANGE.TARGET_REACHED] = this.onTargetReached;
+  this.subscribedEvents[this.entity.eventDispatcher.id][consts.EVENT_ENTITY_STATE_CHANGE.POSITION] = function(e){
+    //cancel attack upon movement
+    if(this.attackSwingId > -1){
+      console.log("Unit moved and attack swing was interupted!");
+      this.entity.manager.core.removeTimer(this.attackSwingId);
+      this.attackSwingId = -1;
+    }
+
+    if(this.unitTarget > -1){
+      var distance = [];
+      var target = this.entity.manager.entities[this.unitTarget];
+      Vec2d.subtract(this.entity.body.position, target.body.position, distance);
+      var distanceLen = Vec2d.len(distance);
+      if(distanceLen < this.attackRange && this.attackSwingId < 0){
+        console.log("Starting to swing at target");
+        this.entity.movement.setTarget(this.entity.body.position);
+        this.attackSwingId = this.entity.manager.core.registerTimer(this.attackTime, this.doSwing, {}, this, false);
+      }
+    }
+  };
 
 
   // this.subscribedEvents[consts.EVENT_ENTITY_STATE_CHANGE.ORIENTATION] = this.entityOrientationChangeListener;
   // this.subscribedEvents[consts.EVENT_ENTITY_STATE_CHANGE.TARGET_REACHED] = this.onTargetReached;
 }
+
+MeleeAttack.prototype.doSwing = function() {
+  console.log("Unit %d was hit by %d", this.unitTarget, this.entity.id);
+  this.attackSwingId = -1;
+};
 
 MeleeAttack.prototype.run = function(data) {
 
@@ -40,7 +70,6 @@ MeleeAttack.prototype.onTargetPositionChange = function(e){
   this.entity.movement.setTarget(e.creator.body.position);
 };
 
-
 MeleeAttack.prototype.onTargetReached = function(e) {
   console.log("%d target reached", this.entity.id);
   this.entity.setDefaultMaterial();
@@ -53,6 +82,9 @@ MeleeAttack.prototype.onTargetReached = function(e) {
 };
 
 MeleeAttack.prototype.destroy = function() {
+  if(this.attackSwingId > -1 ){
+    this.entity.manager.core.removeTimer(this.attackSwingId);
+  }
   
 };
 
