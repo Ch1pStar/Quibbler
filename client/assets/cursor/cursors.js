354b8981909ff7b4dc1dca2f341b1b7541389e0f
Nedelin Yordanov <nedeliny@gmail.com>
2015-10-07 01:00:33 +0300

    Add mouse cursors

---
+++ client/assets/cursor/cursors.js
@@ -0,0 +1,8 @@
+define({
+  'default': 'cursor_default.png',
+  'default_team': 'cursor_default_team.png',
+  'default_enemy': 'cursor_default_enemy.png',
+  'attack_default': 'cursor_attack_default.png',
+  'attack_enemy': 'cursor_attack_enemy.png',
+  'attack_team': 'cursor_attack_team.png',
+});
