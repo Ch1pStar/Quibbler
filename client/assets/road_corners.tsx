70e4fb51a5143c6b4b9264b5c10bc47e24b319e5
Nedelin Yordanov <nedeliny@gmail.com>
2015-10-14 14:02:09 +0300

    Add texture assets

---
+++ client/assets/road_corners.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="road_corners" tilewidth="32" tileheight="32">
+ <image source="road_corners.png" width="384" height="32"/>
+</tileset>
---
+++ client/assets/road_pattern.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="road_pattern" tilewidth="32" tileheight="32">
+ <image source="road_pattern.png" width="481" height="49"/>
+</tileset>
